fa20a1d9f3f7139dd881063abfc4e961dc489535
Hye Chan Lee <hl63@calvin.edu>
2023-09-15 15:46:50 -0400

    most recent prototype

---
+++ components/Button.js
@@ -0,0 +1,34 @@
+//Button.js
+
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+export default function Button({ label, onPress }) {
+  return (
+    
+    <View style={styles.screen}>
+      <TouchableOpacity
+       onPress={onPress}
+       style={styles.roundButton1}>
+       <Text style={styles.text}>{label}</Text>
+     </TouchableOpacity>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+	text: {
+	  fontSize: 25,
+	  color: '#000',
+    textAlign: 'center',
+  },
+  roundButton1: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'orange',
+  },
+});
