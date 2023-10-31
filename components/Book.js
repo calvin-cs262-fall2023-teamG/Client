1090d8e2ef4b3fc30fe878042e3a11b470289c17
Hye Chan Lee <hl63@calvin.edu>
2023-10-31 00:20:17 -0400

    tried to fix physics text

--- components/Book.js
+++ components/Book.js
@@ -5,7 +5,7 @@ const Book = (props) => {
   const { book_name, isbn, author, course_name, price, seller_name, seller_email } = props.bookInfo;
 
   return (
-    <View>
+    <View style={styles.bookcontainer}>
       <Text style={styles.bookname} numberOfLines={1} ellipsizeMode="tail">{book_name}</Text>
       <Text style={styles.price}>${price}</Text>
     </View>
@@ -13,10 +13,12 @@ const Book = (props) => {
 }
 
 const styles = StyleSheet.create({
+  bookcontainer: {
+    maxWidth: '87%',
+  },
   bookname: {
     fontWeight: 'bold',
     fontSize: 17,
-    maxWidth: 320,
   },
 
 });
