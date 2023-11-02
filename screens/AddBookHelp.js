ae21a2c45356ee6f46cda6db74fcb59520997e19
bkh7 <bkh7@calvin.edu>
2023-11-02 15:22:51 -0400

    Build 3

--- screens/AddBookHelp.js
+++ screens/AddBookHelp.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity, Alert, ScrollView, TextInput, Dimensions } from 'react-native'
 import Background from '../components/Background';
+import Animated, {SlideInDown, SlideInUp, SlideInLeft, FadeInLeft, FadeInRight, SlideInRight, BounceInRight, BounceInLeft, FadeInDown, BounceInDown, StretchInX, StretchInY, FadeIn, BounceInUp, ZoomIn, FadeInUp, FlipInYLeft, FlipInYRight, RollInRight, RollInLeft, ZoomInEasyUp, LightSpeedInLeft} from 'react-native-reanimated';
 
 // get height dimensions of the screen 
 
@@ -11,7 +12,7 @@ const AddBookHelp = ({  }) => {
     
 
     return (
-        <View>
+        <Animated.View entering={FadeInUp.duration(500)}>
             <Background></Background>
             <TextFunc textTitle={'Book name: '} textBody={'The title of the book.'}/>
             <TextFunc textTitle={'ISBN #: '} textBody={'ISBN is a book\'s unique id number usually found on the back cover of a book above the bar code on the copyright page.'}/>
@@ -19,7 +20,7 @@ const AddBookHelp = ({  }) => {
             <TextFunc textTitle={'Course name: '} textBody={'The name of the course that this book was used for.'}/>
             <TextFunc textTitle={'Price: '} textBody={<Text>The price that <Text style={{textDecorationLine: 'underline'}}>you</Text> would like to sell your book for.</Text>} />
             <TextFunc textTitle={'Front & back pictures: '} textBody={'A picture of the front and back cover of your book.'}/>
-      </View>
+      </Animated.View>
     )
 }
 
