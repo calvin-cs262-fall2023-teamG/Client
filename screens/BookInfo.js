89fc0e8600ba7313630a731d568f8e4e60296993
Anwesha-P <anwesha.pradhananga@gmail.com>
2023-11-03 15:03:09 -0400

    book info fixed final

--- screens/BookInfo.js
+++ screens/BookInfo.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text, SafeAreaView, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Dimensions, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const { width } = Dimensions.get('window');
-
 const InfoView = ({ name, value, icon }) => {
+    const infoWidth = Dimensions.get('window').width * 0.92 ;
     return (
-        <View style={[styles.info]}>
+        <View style={[styles.info, { width: infoWidth }]}>
             <Icon name={icon} size={20} color="#888181" style={styles.icon} />
             <View>
                 <Text style={{ fontWeight: 'bold', color: "#888181" }}>{name}</Text>
-                <Text style={{ maxWidth: 0.9 * width, color: "#888181" }}>{value}</Text>
+                <Text style={{ color: "#888181" }}>{value}</Text>
             </View>
         </View>
     );
@@ -21,15 +20,15 @@ const BookInfo = ({ route }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View>
-                <InfoView name="Book" icon="book" value={bookInfo.book_name} />
+            <ScrollView>
+                <InfoView name="Book" icon="book" value={bookInfo.title} />
                 <InfoView name="ISBN" icon="hashtag" value={bookInfo.isbn} />
                 <InfoView name="Author" icon="user" value={bookInfo.author} />
                 <InfoView name="Course Name" icon="graduation-cap" value={bookInfo.course_name} />
                 <InfoView name="Price" icon="tags" value={`$${bookInfo.price}`} />
                 <InfoView name="Seller Name" icon="user" value={bookInfo.seller_name} />
                 <InfoView name="Seller Email" icon="envelope" value={bookInfo.seller_email} />
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -42,7 +41,7 @@ const styles = StyleSheet.create({
     },
 
     info: {
-        flex: 1,
+        height: 60,
         marginTop: 10,
         marginBottom: 10,
         flexDirection: 'row',
