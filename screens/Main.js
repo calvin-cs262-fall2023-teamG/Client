de771ec321d96ebf1bf0291424139bc0daaf80ba
bkh7 <bkh7@calvin.edu>
2023-11-07 16:00:09 -0500

    Bug fix

--- screens/Main.js
+++ screens/Main.js
@@ -105,7 +105,7 @@ const Main = () => {
                                 )
                             })
                         )}
-                        <AppEntranceAnimation></AppEntranceAnimation>
+                        
                 </ScrollView>
                 
 
